781650acc5dddd28e15a10e9c40314601e99cc8a
Pegler <pegler@gmail.com>
2011-09-02 16:43:30 -0700

    supports a text button and cross browser

--- jquery.filestyle.js
+++ jquery.filestyle.js
@@ -1,4 +1,4 @@
-tyle File - jQuery plugin for styling file input elements
+/* Style File - jQuery plugin for styling file input elements
  *  
  * Copyright (c) 2007-2009 Mika Tuupola
  *
@@ -16,7 +16,10 @@ tyle File - jQuery plugin for styling file input elements
                 
         /* TODO: This should not override CSS. */
         var settings = {
-            width : 250
+            width : 250,
+            buttonText: 'Choose File',
+            width: 88,
+            height: 28
         };
                 
         if(options) {
@@ -26,30 +29,51 @@ tyle File - jQuery plugin for styling file input elements
         return this.each(function() {
             
             var self = this;
-            var wrapper = $("<div>")
-                            .css({
-                                "width": settings.imagewidth + "px",
-                                "height": settings.imageheight + "px",
+            
+            input = $(this);
+            
+            var outerWrapper = $("<div>").addClass('filestyle');
+            
+            var button = $("<div>").css({
+            	"position": "relative"
+            }).addClass('fileButton');
+            if (settings.image) {
+            	button.css({
+                                "width": settings.width + "px",
+                                "height": settings.height + "px",
                                 "background": "url(" + settings.image + ") 0 0 no-repeat",
                                 "background-position": "right",
                                 "display": "inline",
                                 "position": "absolute",
                                 "overflow": "hidden"
                             });
+            } else {
+                button.text(settings.buttonText).css({
+                                "display": "inline",
+                                "position": "absolute",
+                                "overflow": "hidden",
+                                "width": settings.width + 'px',
+                                "height": settings.height + 'px',
+                                "text-align": "center",
+                                "line-height": settings.height + 'px'
+                });
+            }
                             
-            var filename = $('<input class="file">')
+            var filename = $('<input class="file" READONLY/>')
                              .addClass($(self).attr("class"))
                              .css({
                                  "display": "inline",
-                                 "width": settings.width + "px"
-                             });
+                             }).addClass('fileName');
 
-            $(self).before(filename);
-            $(self).wrap(wrapper);
+			$(self).wrap(outerWrapper);
+			$(self).wrap(button);
+			$(self).parent().parent().prepend(filename);
 
             $(self).css({
-                        "position": "relative",
-                        "height": settings.imageheight + "px",
+                        "position": "absolute",
+                        "top": 0,
+                        "left": 0,
+                        "height": settings.height + "px",
                         "width": settings.width + "px",
                         "display": "inline",
                         "cursor": "pointer",
@@ -67,7 +91,10 @@ tyle File - jQuery plugin for styling file input elements
             };
 
             $(self).bind("change", function() {
-                filename.val($(self).val());
+            	v = $(self).val();
+            	if (v.indexOf('C:\\fakepath\\') == 0)
+            		v = v.substring(12);
+                filename.val(v);
             });
       
         });
